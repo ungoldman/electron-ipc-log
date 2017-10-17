a407473aae7004314c46aa00ad2ccc45db303bb2
Nate Goldman <ungoldman@gmail.com>
2017-10-17 11:55:30 -0700

    fix: ignore internal electron messages

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ var isRenderer = require('is-electron-renderer')
 var electron = require('electron')
 
 function electronIpcLog (log) {
-  var ipc
+  var ipc = isRenderer ? electron.ipcRenderer : electron.ipcMain
+  var scope = isRenderer ? 'RENDERER' : 'MAIN'
+  var oldEmit = ipc.emit
 
-  if (isRenderer) ipc = electron.ipcRenderer
-  else ipc = electron.ipcMain
   if (log == null) log = console.log
 
-  var oldEmit = ipc.emit
-  var scope = isRenderer ? 'renderer' : 'main'
-
   ipc.emit = function (name, e, ...args) {
-    log(`[ipc:${scope}] ${name} ${args.join(', ')}`)
+    // ignore internal electron ipc messages
+    if (name.indexOf('ELECTRON') === -1) {
+      log(`[IPC:${scope}] ${name} ${args.join(', ')}`)
+    }
     oldEmit.call(ipc, name, e, ...args)
   }
 }
